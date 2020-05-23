57aa0a1efe1d07f5980b40c5560f04189c05d7e3
Arpit <arpitfalcon1@gmail.com>
2020-05-23 22:32:37 +0530

    Skipgram model added

--- 04 - Recurrent Neural Networks/Lesson 05 - Embeddings and Word2vec/embeddings/utils.py
+++ 04 - Recurrent Neural Networks/Lesson 05 - Embeddings and Word2vec/embeddings/utils.py
@@ -25,25 +25,6 @@ def preprocess(text):
 
     return trimmed_words
 
-def get_batches(int_text, batch_size, seq_length):
-    """
-    Return batches of input and target
-    :param int_text: Text with the words replaced by their ids
-    :param batch_size: The size of batch
-    :param seq_length: The length of sequence
-    :return: A list where each item is a tuple of (batch of input, batch of target).
-    """
-    n_batches = int(len(int_text) / (batch_size * seq_length))
-
-    # Drop the last few characters to make only full batches
-    xdata = np.array(int_text[: n_batches * batch_size * seq_length])
-    ydata = np.array(int_text[1: n_batches * batch_size * seq_length + 1])
-
-    x_batches = np.split(xdata.reshape(batch_size, -1), n_batches, 1)
-    y_batches = np.split(ydata.reshape(batch_size, -1), n_batches, 1)
-
-    return list(zip(x_batches, y_batches))
-
 
 def create_lookup_tables(words):
     """
@@ -52,8 +33,11 @@ def create_lookup_tables(words):
     :return: A tuple of dicts.  The first dict....
     """
     word_counts = Counter(words)
+    # sorting the words from most to least frequent in text occurrence
     sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
     int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
     vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
 
     return vocab_to_int, int_to_vocab
+
