5c48d3f1f7a5b06af3f1790355f588902ab3c2a4
Arpit <arpitfalcon1@gmail.com>
2020-05-26 08:10:44 +0530

    Updating train.py with train methods

--- 06 - Deploying the model/sagemaker-deployment-master/Project/train/train.py
+++ 06 - Deploying the model/sagemaker-deployment-master/Project/train/train.py
@@ -69,7 +69,30 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     
     # TODO: Paste the train() method developed in the notebook here.
 
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            # the gradients of all optimized variables are cleared
+            optimizer.zero_grad()
+            # forward pass
+            output = model.forward(batch_X)
+            # calculate the batch loss
+            loss = loss_fn(output, batch_y)
+            # backpropagation
+            loss.backward()
+            #optimization
+            optimizer.step()
+            
+            #calculating total loss
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
